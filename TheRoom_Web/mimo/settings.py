7548c3f1bea8c0637d65fdbb85ce1d747ce3254f
HyeW <qorgp13@naver.com>
2021-08-23 19:18:44 +0900

    notice 공사중'

--- TheRoom_Web/mimo/settings.py
+++ TheRoom_Web/mimo/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = secret['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lessontheroom.com','127.0.0.1']
+ALLOWED_HOSTS = ['lessontheroom.com', '127.0.0.1', '*']
 
 
 # Application definition
